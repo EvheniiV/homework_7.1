34a7acab9d65f8652e254c56e9ba1a2fe65ce362
Jenyokgm <jenyokgm@gmail.com>
2023-07-11 23:25:57 +0300

    Final edition

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -3,6 +3,7 @@ package ru.netology.statistic;
 import org.junit.jupiter.api.Test;
 
 import org.junit.jupiter.api.Assertions;
+
 public class StatisticsServiceTest {
 
     @Test
@@ -16,4 +17,17 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+
+    @Test
+    void findMaxSecondTest() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {10, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
+
 }
